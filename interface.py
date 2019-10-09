8cc5a26c25b229de48b668607b7e94ec7f4e3ab0
gfleishman <greg.nli10me@gmail.com>
2019-10-09 16:31:52 -0400

    updated default regularizer

--- interface.py
+++ interface.py
@@ -45,10 +45,10 @@ OUTPUTS
 ARGUMENTS = {
 'transform':'transform to embed - should be displacement not position field',
 'output_directory':'where to write all the amazing results',
-'iterations':'iterations per subsampling level, example: 100x75x25',
+'iterations':'iterations per subsampling level, example: 100x50x25',
 '--mask':'foreground mask, positive where transform should me matched',
 '--time_steps':'number of time steps in geodesic shooting integration; default 6',
-'--regularizer':'AxBxCxD for metric (A*divgrad + B*graddiv + C)^D; default 3x0x1x3',
+'--regularizer':'AxBxCxD for metric (A*divgrad + B*graddiv + C)^D; default 12x0x1x2',
 '--regularizer_balance':'S in (1/S^2) * image-match + regularizer; default 0.03',
 '--gradient_step':'initial gradient descent step size; default 0.001',
 '--optimization_tolerance':'factor by which energy may *increase* between iterations; default 1.15'
@@ -57,7 +57,7 @@ ARGUMENTS = {
 # OPTIONS
 OPTIONS = {a:{'help':ARGUMENTS[a]} for a in ARGUMENTS.keys()}
 OPTIONS['--time_steps'] = {**OPTIONS['--time_steps'], 'default':'6'}
-OPTIONS['--regularizer'] = {**OPTIONS['--regularizer'], 'default':'3x0x1x3'}
+OPTIONS['--regularizer'] = {**OPTIONS['--regularizer'], 'default':'12.0x0x1x2'}
 OPTIONS['--regularizer_balance'] = {**OPTIONS['--regularizer_balance'], 'default':'.03'}
 OPTIONS['--gradient_step'] = {**OPTIONS['--gradient_step'], 'default':'.001'}
 OPTIONS['--optimization_tolerance'] = {**OPTIONS['--optimization_tolerance'], 'default':'1.15'}
